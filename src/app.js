44ecb9c33c28876e7b9253f73406f6f8a06a0b30
Navivarman <navi200511@gmail.com>
2024-11-30 15:54:47 +0530

    wildcard-error handling

--- src/app.js
+++ src/app.js
@@ -11,4 +11,22 @@ app.use("/admin/getAllData",adminAuth,(req,res) =>{
 app.use("/user/login",(req,res) =>{
     res.send("User login is sent");
 })
+
+
+app.use("/test",(req,res) =>{
+    try{
+        throw new Error("Test error");
+        res.send("successful")
+    }catch(err){
+        res.status(500).send({message:err.message})
+    }
+})
+
+app.use("/",(err,req,res,next) =>{
+    if(err){
+        res.status(500).send("Something went wrong");
+    }
+})
+
+
 app.listen(7777);
