bf1a49341ed767a8ce4b5a10a04eb1787076898c
Navivarman <navi200511@gmail.com>
2024-11-30 12:47:47 +0530

    exploring the GET,POST and DELETE request

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
 
+app.get("/test",(req,res) =>{
+    res.send({firstname:"navi" ,lastname:"varman"})
+})
+app.post("/test",(req,res) =>{
+    res.send("POST is successfully executed")
+})
+app.patch("/test",(req,res) =>{
+    res.send("PATCH is successfully executed")
+})
+app.put("/test",(req,res) =>{
+    res.send("PUT is successfully executed")
+})
+app.delete("/test",(req,res) =>{
+    res.send("DELETE is successfully executed")
+})
 app.use("/test",(req,res) =>{
     res.send("Hello Test ");
 })
+
 app.use('/hello',(req,res) =>{
     res.send("Hello Hello");
+})
+app.use('/',(req,res) =>{
+    res.send("Hello Home page");
 })
 app.listen(7777);
