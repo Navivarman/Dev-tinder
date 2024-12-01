5260395a433c9bacb372449390a96ef07a751424
Navivarman <navi200511@gmail.com>
2024-12-01 15:21:13 +0530

    Storing and retreving dynamic data in database

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const {connectDB} = require('./config/database')
-const User = require('./models/User');
+const User = require('./models/user');
 const app = express();
+app.use(express.json())
 app.post("/signup", async(req,res) =>{
-    const user = new User({
-        firstName:"Navi",
-        lastName:"Varman",
-        email:"navi@gmail.com",
-        password:"123456",
-         })
+    const user = new User(req.body)
     try{
         await user.save();
         res.send("Successfully posted")
@@ -16,12 +12,33 @@ app.post("/signup", async(req,res) =>{
         res.status(400).send("Error :",err)
     }
 })
+
+app.get("/user",async(req,res) =>{
+    try{
+        const userEmail = req.body.email;
+        const userId =  await User.findOne({email : userEmail});
+        res.send(userId)
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+
+})
+app.get("/feed",async(req,res) =>{
+    try{
+        const userEmail = req.body.email;
+        const userId =  await User.find({});
+        res.send(userId)
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+
+})
 connectDB().then(() =>{
     console.log("Database is connected successfully");
     app.listen(7777,() =>{
         console.log("Server is running on port 7777");
     });
 }).catch((err) =>{
-    console.log("Error in connecting to the database");
+    console.log(err);
 })
 
